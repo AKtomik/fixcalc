c3d285cedea45e3d53dc2692f9f66b90cbbeac1b
biscuitlac <tom.hennequin54@gmail.com>
2025-01-28 14:50:49 +0100

    WED

--- appweb.py
+++ appweb.py
@@ -10,11 +10,14 @@ def index():
 @app.route('/calculate', methods=['POST'])
 def calculate():
     expression = request.form.get('expression')
+    print(expression)
     try:
         result = calcul(expression)  
         return render_template('index.html', result=result, error=None)
     except Exception as e:
         return render_template('index.html', result=None, error="Invalid expression")
-
+@app.route('/exmeple', methods=['POST'])
+def index():
+    return render_template('index.html', result=None, error=None)
 if __name__ == '__main__':
     app.run(debug=True)
