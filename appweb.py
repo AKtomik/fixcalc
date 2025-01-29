00528c2f4796c2f1e95c1b27b0de8c83d5555e15
biscuitlac <tom.hennequin54@gmail.com>
2025-01-29 20:08:07 +0100

    web 5

--- appweb.py
+++ appweb.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-from calculate import calcul 
+from calculate import calcul  
 
 app = Flask(__name__)
 
@@ -10,14 +10,15 @@ def index():
 @app.route('/calculate', methods=['POST'])
 def calculate():
     expression = request.form.get('expression')
-    print(expression)
     try:
         result = calcul(expression)  
         return render_template('index.html', result=result, error=None)
-    except Exception as e:
+    except Exception:
         return render_template('index.html', result=None, error="Invalid expression")
+
 @app.route('/exmeple', methods=['POST'])
-def index():
-    return render_template('index.html', result=None, error=None)
+def example():
+    return render_template('exmeple.html')  
+
 if __name__ == '__main__':
     app.run(debug=True)
