b06987b30ecdd566ccbcb16435f0afb5e0722fe0
Evann (lw) <--remove>
2025-03-04 14:29:37 +0100

    resluts class

--- calculate.py
+++ calculate.py
@@ -1,5 +1,12 @@
 from fixClass import postFix
+from resluts import FractionResult
+
 
 def calcul(string):
     return f"{postFix(string).calculate()}"
 t=calcul("(33*9)/(11*27)+1")
+
+print(FractionResult(1,2))
+print(FractionResult(2,20))
+print(FractionResult.create_from_float(.5))
+print(FractionResult.create_from_string(".12"))
