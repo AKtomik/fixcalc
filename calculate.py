eec34244874e454eb39b5a60a7dc4acd54d8d2c4
AKtomik <97125853+AKtomik@users.noreply.github.com>
2025-03-24 23:10:18 +0100

    UnitsResult class

--- calculate.py
+++ calculate.py
@@ -1,11 +1,19 @@
 from fixClass import postFix
-from resluts import FractionResult, RoundResult
+from resluts import FractionResult, RoundResult, UnitsResult
 
 
 def calcul(string, resultTypeClass=RoundResult):
     return f"{postFix(string, resultTypeClass).calculate()}"
 #t=calcul("(33*9)/(11*27)+1")
 
+#print(UnitsResult.create_as_unit("x")+UnitsResult.create_as_unit("y"))
+print(UnitsResult.create_as_unit("x")*UnitsResult.create_as_unit("y"))
+print(UnitsResult.create_as_unit("x")*UnitsResult.create_as_unit("y"))
+print(UnitsResult.create_as_unit("x")*UnitsResult.create_as_unit("y"))
+print(UnitsResult.create_from_string("-5")*(UnitsResult.create_as_unit("x")*UnitsResult.create_as_unit("x")*UnitsResult.create_as_unit("x")*UnitsResult.create_as_unit("x")*UnitsResult.create_as_unit("x")))
+print(UnitsResult.create_as_unit("x")*UnitsResult.create_as_unit("x")*UnitsResult.create_as_unit("x")*UnitsResult.create_as_unit("x")*UnitsResult.create_as_unit("x"))
+#print(calcul("1/3", UnitsResult))
+print(calcul("(33*9)/(11*28)+1", UnitsResult))
 print(calcul("(33*9)/(11*28)+1", RoundResult))
 print(calcul("(33*9)/(11*28)+1", FractionResult))
 
